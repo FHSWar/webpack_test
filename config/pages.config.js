2df87c538e80efe5c7e4f7145302dbab165ddad0
FHSWar <fhswar0504@163.com>
2021-12-18 21:42:01 +0800

    调整 page.config.js 逻辑避免 .DS_Store 文件导致崩溃

--- config/pages.config.js
+++ config/pages.config.js
@@ -13,19 +13,21 @@ pagesArr.map(page => {
     // 每个页面的文件夹下可以含有一个自己的index.html，如果有会根据这个模板进行build
     const pageTemplate = pagePath + '/index.html'
     const pageEntry = pagePath + '/index.js'
-    
-    // 如果文件夹下没有制定的模板，就报错
-    if (!fs.existsSync(pageTemplate)) throw new Error('对应目录下没有 index.html')
 
-    // 根据路径生成 HTMLWebpackPlugin 对象
-    const htmlPlugin = new HTMLWebpackPlugin({
-        filename: `${page}.html`, // 生成到dist目录下的 html 文件名称
-        template: pageTemplate, // 模板文件，不同入口可以根据需要设置不同模板
-        chunks: [page], //引入对应的js(对应(entry)中的入口文件)
-    });
+    if (fs.existsSync(pageTemplate)) {
+        // 根据路径生成 HTMLWebpackPlugin 对象
+        const htmlPlugin = new HTMLWebpackPlugin({
+            filename: `${page}.html`, // 生成到dist目录下的 html 文件名称
+            template: pageTemplate, // 模板文件，不同入口可以根据需要设置不同模板
+            chunks: [page] //引入对应的js(对应(entry)中的入口文件)
+        });
 
-    HTMLPlugins.push(htmlPlugin);
-    Entries[page] = [pageEntry]
+        HTMLPlugins.push(htmlPlugin);
+        Entries[page] = [pageEntry]
+    } else {
+        // 如果文件夹下没有制定的模板，就打印一下文件夹名
+        console.log(`${pageTemplate} 目录下没有 index.html`)
+    }
 })
 
 module.exports = [HTMLPlugins, Entries]
