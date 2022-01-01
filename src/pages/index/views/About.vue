14e02bbdc7e6ae6d6c16f79322c001578be126bc
FHSWar <fhswar0504@163.com>
2022-01-01 20:35:53 +0800

    feat: 引入 ajv 并解决入口页过大的问题

--- src/pages/index/views/About.vue
+++ src/pages/index/views/About.vue
@@ -4,12 +4,28 @@
   </div>
 </template>
 
-<script>
-export default {
-	name: 'ComponentAbout',
-	mounted() {
-		console.log('ComponentAbout')
-	}
+<script lang="ts" setup>
+import Ajv from 'ajv'
+const ajv = new Ajv() // options can be passed, e.g. {allErrors: true}
+const schema = {
+	type: 'object',
+	properties: {
+		foo: {type: 'integer'},
+		bar: {type: 'string'}
+	},
+	required: ['foo'],
+	additionalProperties: false
+}
+const data = {
+	foo: 1,
+	bar: 'abc'
+}
+const validate = ajv.compile(schema)
+const valid = validate(data)
+if (!valid) {
+	console.log(validate.errors)
+} else {
+	console.log('valid', valid)
 }
 </script>
 
